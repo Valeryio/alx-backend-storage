498ce9257a8d4de05ca663c9c9df74b3529e3fb0
Valeryio <tommagoma23@gmail.com>
2024-10-23 10:23:56 +0000

    commit the task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""This is a simple exercice on redis database
+"""
+import uuid
+import redis
+
+
+class Cache:
+
+    def __init__(self):
+        self._redis = redis.Redis(host="localhost")
+
+    @property
+    def redis(self):
+        return self._redis
+
+    @redis.setter
+    def redis(self, redis_instance):
+        self._redis = redis_instance
+
+    def store(self, data):
+        """method to store a data
+        param: data
+        return: a string
+        """
+        # generate a random uuid
+        random_key = uuid.uuid1()
+        self.redis.set(str(random_key), data)
+        return str(random_key)
